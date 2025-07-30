848b288696194a0ae7e3528ca8d1683795421409
Pedro <91034362+toshicoxyz@users.noreply.github.com>
2025-07-30 09:07:39 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Plan & Cook',
-  description: 'Plan & Cook',
+  title: 'Plan - Cook',
+  description: 'Plan - Cook',
 }
 
 export default function RootLayout({
